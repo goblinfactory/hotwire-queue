3868162dfebd14cf7999d251b6e8d5d562bae824
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-05-21 17:29:34 +0000

    Additional changes required to migrate unit and acceptance tests from client project to open source project. Moving bits and pieces across at a time. Preparing for being able to unit test the Hotwire httpModule classes.

---
+++ trunk/V.0.2.4/Icodeon.Hotwire.Tests/Framework/UnitTest.cs
@@ -0,0 +1,22 @@
+﻿using Icodeon.Hotwire.Framework;
+using Icodeon.Hotwire.Framework.Providers;
+
+namespace Icodeon.Hotwire.Tests.Framework
+{
+    public class UnitTest
+    {
+
+        public HotwireFilesProvider FilesProvider { get; private set; }
+        public HotLogger Logger { get; private set; }
+
+        public UnitTest()
+        {
+            Logger = HotLogger.GetLogger(LogFiles.UnitTests);
+            Logger.Trace("UnitTest constructor. (base class for most unit tests.)");
+            FilesProvider = HotwireFilesProvider.GetFilesProviderInstance(HotLogger.NullLogger);
+        }
+
+
+
+    }
+}
