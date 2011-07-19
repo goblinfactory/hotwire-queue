5bef11353731856627984dfaa62ef2140ade4278
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-07-19 15:03:10 +0000

    Solution was missing reference to TestFramework project. Structuremap was being referenced from the wrong location.

--- V.0.2.4/Icodeon.Hotwire.TestFramework/AcceptanceTest.cs
+++ V.0.2.4/Icodeon.Hotwire.TestFramework/AcceptanceTest.cs
@@ -11,10 +11,6 @@ namespace Icodeon.Hotwire.TestFramework
 
         public AcceptanceTest() 
         {
-            // CI?
-            // TODO: need to include test to see what server this code has been deployed on! We will assume for now that 
-            if (!DeploymentEnvironment.IsDEBUG) throw new Exception("RELEASE mode testing currently not supported on dev machines. If these tests will run on CI server then this test needs to be reversed!"); 
-            
             ConsoleWriter = new ConsoleWriter();
         }
 
