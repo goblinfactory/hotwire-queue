3b7b49be3aba6d023366de37ad16f4b529708ca4
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-07-20 09:29:11 +0000

    commented out "can you see me" : useful class to commit from time to time if I need to test the CI build. Can be deleted later on if not valuable.

--- V.0.2.4/Icodeon.Hotwire.Tests/CanYouSeeMe.cs
+++ V.0.2.4/Icodeon.Hotwire.Tests/CanYouSeeMe.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using Icodeon.Hotwire.Framework.Configuration;
-using NUnit.Framework;
+﻿//using System;
+//using Icodeon.Hotwire.Framework.Configuration;
+//using NUnit.Framework;
 
-namespace Icodeon.Hotwire.Tests.Internal
-{
-    [TestFixture]
-    public class CanYouSeeMe
-    {
+//namespace Icodeon.Hotwire.Tests.Internal
+//{
+//    [TestFixture]
+//    public class CanYouSeeMe
+//    {
 
-        [Test]
-        public void YabbaDabbaDooo()
-        {
-#if DEBUG
-            throw new Exception("Can you this? Apparently this is a debug configuration!");
-#else
-            throw new Exception("Nope... we are NOT in debug mode...most likely RELEASE, will check further later.");
-#endif
-        }
-    }
-}
+//        [Test]
+//        public void YabbaDabbaDooo()
+//        {
+//#if DEBUG
+//            throw new Exception("Can you this? Apparently this is a debug configuration!");
+//#else
+//            throw new Exception("Nope... we are NOT in debug mode...most likely RELEASE, will check further later.");
+//#endif
+//        }
+//    }
+//}
