160e51f67021aed29fae1af79bb197fc341d4d06
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-07-20 09:05:12 +0000

    test to see if the test runner picks this up

---
+++ V.0.2.4/Icodeon.Hotwire.Tests/CanYouSeeMe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Icodeon.Hotwire.Framework.Configuration;
+using NUnit.Framework;
+
+namespace Icodeon.Hotwire.Tests.Internal
+{
+    [TestFixture]
+    public class CanYouSeeMe
+    {
+
+        [Test]
+        public void YabbaDabbaDooo()
+        {
+            throw new Exception("Can you this?");
+        }
+    }
+}
