772ff704bffa5c2c887bd0d3838077c26652e767
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-07-21 17:16:43 +0000

    update unit test to echo to console so that Trace, and TraceTitle will output to test runners reports.

--- V.0.2.4/Icodeon.Hotwire.TestFramework/UnitTest.cs
+++ V.0.2.4/Icodeon.Hotwire.TestFramework/UnitTest.cs
@@ -9,6 +9,7 @@ namespace Icodeon.Hotwire.TestFramework
         public UnitTest()
         {
             Logger = HotLogger.GetCurrentClassLogger();
+            Logger.EchoToConsole = true;
         }
 
         public void Trace(string message, params object[] parameters)
