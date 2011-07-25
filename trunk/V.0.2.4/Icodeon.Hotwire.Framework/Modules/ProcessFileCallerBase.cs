63df76a4c4d99c932c9eaaf9c41a26cf679de279
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-07-25 16:01:16 +0000

    can now handle the case if the file processor locks the file before throwing an exception.

---
+++ trunk/V.0.2.4/Icodeon.Hotwire.Framework/Modules/ProcessFileCallerBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Icodeon.Hotwire.Framework.Modules
+{
+    public abstract class ProcessFileCallerBase
+    {
+
+        public abstract void CallProcessFileWaitForComplete(string trackingNumber);
+    }
+}
