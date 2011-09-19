904ffd68071532701db31ad9a8f82d207f1950fa
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-09-19 09:08:49 +0000

    still broken build, moving to new machine.

---
+++ trunk/V.0.2.4/Icodeon.Hotwire.Framework/Repository/IMacSaltDAL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Icodeon.Hotwire.Framework.Utils;
+
+namespace Icodeon.Hotwire.Framework.Repositories
+{
+    public interface IMacSaltDAL
+    {
+        void CacheRequest(string hotwireMac, Guid salt, string startString, int msToExpire);
+        bool RequestsExists(string hotwireMac, Guid salt);
+        void RemoveExpiredItems(IDateTime dateTime);
+        int UrlMaxLength { get; }
+        IRepository Repository { get; }
+    }
+
+}
