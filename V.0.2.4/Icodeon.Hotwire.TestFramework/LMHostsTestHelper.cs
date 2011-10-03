122e9acc86a449dd50c293f1a5989d096364a617
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-10-03 16:25:23 +0000

    Added IDisposable LHMosts ( safely create tests that modify LMHosts, creates backup and keeps LMHosts safe) new : DeployManager and VersionedSiteManager allows you to deploy and goLive easily when testing, see GoLiveRestfulServiceTests.cs

---
+++ V.0.2.4/Icodeon.Hotwire.TestFramework/LMHostsTestHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using FluentAssertions;
+
+namespace Icodeon.Hotwire.TestFramework
+{
+    public class LMHostsTestHelper
+    {
+        public void ShouldResolveToLocalhost(string hostname)
+        {
+            var adresses = System.Net.Dns.GetHostAddresses(hostname);
+            var address = adresses.FirstOrDefault(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork).ToString();
+            address.Should().Be("127.0.0.1");
+        }
+
+        public void ShouldNotResolveHostName(string hostname)
+        {
+            Action action = () => System.Net.Dns.GetHostAddresses(hostname);
+            action.ShouldThrow<SocketException>();
+        }
+    }
+}
