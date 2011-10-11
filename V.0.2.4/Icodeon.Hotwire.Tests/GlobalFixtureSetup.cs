835bcece32e8f3585a0f6686d1dc727431f67a82
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-10-11 13:26:42 +0000

    Added facility to shell to execute the creation of the database. Not wired into the tests yet. Moved the setup and teardown of the database to be a once-off per full set of tests, instead of just for this one test.

---
+++ V.0.2.4/Icodeon.Hotwire.Tests/GlobalFixtureSetup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Icodeon.Hotwire.Framework.DAL;
+using Icodeon.Hotwire.Framework.Deployment;
+using Icodeon.Hotwire.Framework.Providers;
+using Icodeon.Hotwire.Framework.Utils;
+using NLog;
+using NUnit.Framework;
+
+namespace Icodeon.Hotwire.Tests
+{
+    [TestFixture]
+    public class GlobalFixtureSetup
+    {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        [TestFixtureSetUp]
+        public void SetUp()
+        {
+            _logger.Debug("");
+            _logger.Debug("GlobalFixtureSetup.SetUp()");
+            _logger.Debug("--------------------------");
+            _logger.Debug("");
+            using (var db = new HotwireContext(ConnectionStringManager.HotwireConnectionString))
+            {
+                if (!db.DatabaseExists()) db.CreateDatabase();
+            }
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            _logger.Debug("");
+            _logger.Debug("GlobalFixtureSetup.TearDown()");
+            _logger.Debug("--------------------------");
+            _logger.Debug("");
+            using (var db = new HotwireContext(ConnectionStringManager.HotwireConnectionString))
+            {
+                db.DeleteDatabase();
+            }
+        }
+
+
+    }
+}
---
+++ V.0.2.4/Icodeon.Hotwire.Tests/Sql/CreateHotwireUnitTest.bat
@@ -0,0 +1,5 @@
+sqlcmd -S buildserver1\sqlexpress -i CreateHotwireUnitTest.sql
+
+echo 
+echo "done. Press enter to quit"
+pause
