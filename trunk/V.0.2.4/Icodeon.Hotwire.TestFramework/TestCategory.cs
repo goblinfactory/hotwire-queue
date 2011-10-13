04ce3eeb6dfda83f45041f83fbbf3d9a54d2ddc5
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-10-13 12:12:59 +0000

    started thinking about tagging tests [category("xx")] if a test is not categorised, then we assume it's fast, and can work offline. I still need to go through all the existing tests, and group them. Am considering moving them to different test projects.
    Avaiable categories are "Online" and "Slow".

---
+++ trunk/V.0.2.4/Icodeon.Hotwire.TestFramework/TestCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Icodeon.Hotwire.TestFramework
+{
+    public static class TestCategory
+    {
+        // is a slow test
+        public const string Slow = "Slow";
+        // requires a live internet connection to be able to run
+        public const string Online = "Online";
+
+
+        // anything NOT marked as ONLINE or SLOW is by definition "OFFLINE" and FAST!
+    }
+}
