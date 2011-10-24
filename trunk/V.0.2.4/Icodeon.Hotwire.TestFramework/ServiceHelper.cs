f344dddfd1577eefaa8249d79b22047710fd52b8
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-10-24 17:30:51 +0000

    can now install and remove services programmatically.

--- trunk/V.0.2.4/Icodeon.Hotwire.TestFramework/ServiceHelper.cs
+++ trunk/V.0.2.4/Icodeon.Hotwire.TestFramework/ServiceHelper.cs
@@ -1,50 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration.Install;
 using System.Linq;
+using System.Security.Principal;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 
 namespace Icodeon.Hotwire.TestFramework
 {
     public class ServiceHelper
     {
         private readonly string _serviceName;
+        private readonly string _assemblyName;
+        private ServiceController _serviceController;
 
-        public ServiceHelper(string serviceName)
+        public ServiceHelper(string serviceName, string assemblyName)
         {
+            if (!IsUserAdministrator()) throw new ApplicationException("You must be member of 'admin' group to use ServiceHelper.");
             _serviceName = serviceName;
+            _assemblyName = assemblyName;
+            ServiceController sc = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == _serviceName);
         }
 
-        public void UninstallIfInstalled()
+        public static bool IsInstalled(string serviceName)
         {
-            throw new NotImplementedException();
+            var service = ServiceController.GetServices().FirstOrDefault(s => s.ServiceName == serviceName);
+            return service != null && service.ServiceName == serviceName;
         }
 
-        private void InstallService()
+
+
+        public static void InstallService(string assemblyName, string serviceName)
         {
-            throw new NotImplementedException();
+            if (IsInstalled(serviceName)) return;
+            ManagedInstallerClass.InstallHelper(new string[] { assemblyName });
         }
 
-        public bool Installed
+        public void UnInstallService()
         {
-            get
+            if (!IsInstalled(_serviceName)) return;
+            ManagedInstallerClass.InstallHelper(new string[] { _assemblyName, "/u" });
+        }
+
+
+        public void Start()
+        {
+            _serviceController.Start();
+            int cnt = 0;
+            while(_serviceController.Status == ServiceControllerStatus.StartPending)
             {
-                try
-                {
-                    var controller = new ServiceController(_serviceName);
-                    string name = controller.DisplayName;
-                    return true;
-                }
-                catch (InvalidOperationException op)
-                {
-                    if (op.Message.Contains("Service does not exist")) return false;
-                    throw;
-                }
-
-
+                Thread.Sleep(100 * cnt + 100); // wait longer and longer
+                if (cnt++ > 5) throw new ApplicationException("timed out waiting for service to start, waited approximately 10 seconds.");
             }
         }
 
+        public void Stop()
+        {
+            _serviceController.Stop();
+            int cnt = 0;
+            while (_serviceController.Status == ServiceControllerStatus.StopPending)
+            {
+                Thread.Sleep(100 * cnt + 100); // wait longer and longer
+                if (cnt++ > 5) throw new ApplicationException("timed out waiting for service to stop, waited approximately 10 seconds.");
+            }
+            
+        }
+
+        private bool IsUserAdministrator()
+        {
+            try
+            {
+                return new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
 
     }
+
 }
