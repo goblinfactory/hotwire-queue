54630c786a1f4e60d34c65b36b2afd1dae5b167e
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-10-25 16:30:14 +0000

    IFileUrlSignatureProvider.cs - if the enqueue request indicates that the external resource file is secured, (and security is set to "consumer") , then hotwire will call IFileUrlSignatureProvider to sign the request, prior to downloading the resource.

---
+++ trunk/V.0.2.4/Icodeon.Hotwire.Contracts/IFileUrlSignatureProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Icodeon.Hotwire.Contracts
+{
+    public interface IFileUrlSignatureProvider
+    {
+        Uri SignUrl(
+            Uri urlToSign,
+            NameValueCollection requestParams);
+    }
+}
