beb9c8017320668275dcaeb2e2215a614128f295
goblinfactory@gmail.com <goblinfactory@gmail.com@cdef0934-145c-4ee3-c4a6-22d994fa6027>
2011-11-02 18:59:14 +0000

    Removed fileprocessor Module, and instead added an extra "method/action" on the QueueModule. Shortens the hotwire configuration by removing the extra config section. Added single line that you can use to setup the folders. HotwireFilesProvider.CreateFoldersIfNotExist() will create all the folders needed, in the locations specified in app_data/hotwire-folders.config

--- V.0.2.4/Icodeon.Hotwire.Framework/Modules/FileProcessorModule.cs
+++ V.0.2.4/Icodeon.Hotwire.Framework/Modules/FileProcessorModule.cs
@@ -13,97 +13,41 @@ using NLog;
 
 namespace Icodeon.Hotwire.Framework.Modules
 {
-    public class FileProcessorModule : ModuleBase
+    // rename file to FileProcessorHelper, this isnt a module anymore!
+
+    public static class FileProcessorModule 
     {
         private static Logger _logger = LogManager.GetCurrentClassLogger();
 
-        protected override string GetConfigurationSectionName()
+        public static EnqueueRequestDTO ProcessFile(string trackingNumber, QueueDal dal)
         {
-            return ProcessFileConfiguration.ProcessFileSectionName; 
-        }
-
-        public override object ProcessRequest(ParsedContext context)
-        {
-            DebugContract.Ensure(
-                                 ()=> context.Match != null,
-                                 () => context.ModuleConfig != null,
-                                 () => context.MediaInfo != null);
+            // boundvariables("TRACKING-NUMBER")
             
-            _logger.Trace("ProcessRequest, {0}, {1}", context.ModuleConfig.Action, context.Url);
-
-            IModuleEndpoint moduleConfig = context.ModuleConfig;
-            switch (moduleConfig.Action)
+            _logger.Trace("ProcessFile(trackingNumber:='{0}')",trackingNumber);
+            var fileProvider = HotwireFilesProvider.GetFilesProviderInstance();
+            // ADH: queue dal should be injected
+            //var dal = new QueueDal(fileProvider);
+            EnqueueRequestDTO dto = dal.GetByTrackingNumber(trackingNumber);
+            var processor = new ProviderFactory().CreateFileProcessor();
+            var parameters = dto.ToUnderScoreIcodeonCCPNamedNameValueCollectionPlusExtraHotwireParamsAndAnyExtraParamsPostedByClient();
+            _logger.TraceParameters(parameters);
+            _logger.Trace("process the file ...");
+            try
             {
-
-                case ActionProcessFile:
-                    _logger.Trace("SmokeTestModule -> PROCESS-FILE");
-                    string trackingNumber = context.Match.BoundVariables["TRACKING-NUMBER"];
-                    _logger.Trace("Tracking Number = " + trackingNumber);
-                    _logger.Trace("Security for endpoint is " + moduleConfig.Security);
-                    if (moduleConfig.Security==SecurityType.localonly)
-                    {
-                        if (!context.Url.IsLoopback)
-                            throw new HttpModuleException(HttpStatusCode.Forbidden, "Remote connections not allowed.");
-                    }
-                    var fileProvider = HotwireFilesProvider.GetFilesProviderInstance();
-                    // ADH: queue dal should be injected
-                    var dal = new QueueDal(fileProvider);
-                    EnqueueRequestDTO dto = dal.GetByTrackingNumber(trackingNumber);
-                    var processor = new ProviderFactory().CreateFileProcessor();
-                    var parameters = dto.ToUnderScoreIcodeonCCPNamedNameValueCollectionPlusExtraHotwireParamsAndAnyExtraParamsPostedByClient();
-                    _logger.TraceParameters(parameters);
-                    _logger.Trace("process the file ...");
-                    try
-                    {
-                        processor.ProcessFile(dto.GetTrackingNumber(), dto.TransactionId, parameters);
-                        // all we need to do is move the file as the last step, i.e. change the status and save. Needs to be transactional
-                        // for now we'll leave that up to the caller to do.
-                    }
-                    catch (Exception ex)
-                    {
-                        fileProvider.MoveFileAndSettingsFileFromProcessingFolderToErrorFolderWriteExceptionFile(dto.ResourceFile,ex);
-                        //RAISE EXCEPTION
-                        throw;
-                    }
-                    return dto;
-
-                case ActionEcho:
-                    string message = context.Match.BoundVariables["SAY"];
-                    return message;
-
-                default: throw new HttpModuleException(HttpStatusCode.BadRequest, moduleConfig.Action + " is not a supported action.");
+                processor.ProcessFile(dto.GetTrackingNumber(), dto.TransactionId, parameters);
+                // all we need to do is move the file as the last step, i.e. change the status and save. Needs to be transactional
+                // for now we'll leave that up to the caller to do.
             }
+            catch (Exception ex)
+            {
+                fileProvider.MoveFileAndSettingsFileFromProcessingFolderToErrorFolderWriteExceptionFile(dto.ResourceFile,ex);
+                //RAISE EXCEPTION
+                throw;
+            }
+            return dto;
         }
 
         public const string ActionProcessFile = "PROCESS-FILE";
-
-
-        public const string SlashEncodingToken = "~";
-
-        public static string EscapeSlashes(string src)
-        {
-            
-            // deviation (by design for security reasons) in .NET 4 Uri class from the RFC spec
-            // which means you cant (easily) pass slash as an uri parameter, 
-            // which is the reason for the tokenising of the slash here. 
-            // see: http://stackoverflow.com/questions/591694/url-encoded-slash-in-url for more info.
-            return src.Replace("/", SlashEncodingToken);
-        }
-
-        public static string UnescapeSlashes(string src)
-        {
-            return src.Replace(SlashEncodingToken, "/");
-        }
-
-
-        public override IEnumerable<string> ActionNames
-        {
-            get { return new[] { ActionVersion, ActionProcessFile }; }
-        }
-
-
-
-
 
     } 
 } 
