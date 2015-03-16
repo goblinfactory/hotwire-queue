0f8bf0131b14354f7fab9f8f4ee0fcdf50e887b6
Alan Hemmings <goblinfactory@gmail.com>
2015-03-16 21:03:41 +0000

    migrated project to work with vs2013 and updated System.Net.Http (was in alpha? when hotwire was first being written.) THis is a quick and dirty upgrade, only in order that the project can be built and tests can be run, so that I can use the project as a reference. I enjoyed writing the acceptance tests, testing really normally hard to test code.

--- V.0.2.4/Icodeon.Hotwire.Tests/Internal/Website.cs
+++ V.0.2.4/Icodeon.Hotwire.Tests/Internal/Website.cs
@@ -3,6 +3,7 @@
     public static class TestAspNet
     {
         public static string RootUri = @"http://localhost:44144/";
+        //public static string RootUri = @"http://localhost/Icodeon.Hotwire.TestAspNet/";
         public static string MockCDN = "MockCDN/";
     }
 }
